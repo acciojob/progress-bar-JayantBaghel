466c74c62ea3eb8f11c340241331141e786c48c5
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-28 10:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const circles = document.querySelectorAll(".circle");
+    const prevBtn = document.getElementById("prev");
+    const nextBtn = document.getElementById("next");
+
+    let currentStep = 1;
+
+    updateButtons();
+
+    prevBtn.addEventListener("click", () => {
+        if (currentStep > 1) {
+            currentStep--;
+            updateButtons();
+        }
+    });
+
+    nextBtn.addEventListener("click", () => {
+        if (currentStep < circles.length) {
+            currentStep++;
+            updateButtons();
+        }
+    });
+
+    function updateButtons() {
+        circles.forEach((circle, index) => {
+            if (index < currentStep - 1) {
+                circle.classList.add("active");
+            } else {
+                circle.classList.remove("active");
+            }
+        });
+
+        prevBtn.disabled = currentStep === 1;
+        nextBtn.disabled = currentStep === circles.length;
+    }
+});
